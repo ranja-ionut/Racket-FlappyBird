ed21d6597fc896d443556a44b5a84f59cba6cc56
ranja-ionut <64152047+ranja-ionut@users.noreply.github.com>
2021-03-09 17:44:53 +0200

    Update abilities.rkt

--- abilities.rkt
+++ abilities.rkt
@@ -12,8 +12,6 @@
 (require lang/posn)
 (require 2htdp/image)
 
-; Imaginea si range-ul în care vor aparea abilitațile
-; Nu modificați
 (define POSITION_RANGE '((300 2000) (30 550)))
 (define (hourglass color) (underlay
  (rectangle 40 40 "solid" color)
@@ -46,7 +44,6 @@
 
 ; Va parcurge abitatile și pentru cele care au poziția null va asigna
 ; una aletorie.
-; Folosiți random-position
 (define (position-abilities abilities)
   (map (λ(ability) (cond [(null? (get-ability-pos ability)) (struct-copy abilities-struct ability [pos (random-position POSITION_RANGE)])]
                          [else ability]
@@ -54,8 +51,6 @@
   )
 
 ; Fiecare abilitate are o funcție next care modifica stare jocului
-; Compuneti toate funcțiile next în una singură
-; Hint: compose
 (define (compose-abilities L)
   (foldl (λ(ability acc) (compose (λ(x) (+ x 0)) (get-ability-next ability))) null L)
   )
@@ -63,8 +58,6 @@
 ; Primiște o listă de abilități inițiale, un număr n
 ; și o listă cu toate abilități posibile.
 ; Va adauga elemente aleatoare la lista inițială pană aceasta are lungimea n
-; Atentie n poate fi chiar si 0 cand vrem sa jucam fara nicio abilitate.
-; Folosiți choice-abilities.
 (define (fill-abilities initial n abilities)
   (cond [(zero? n) null]
         [(>= (length initial) n) (position-abilities initial)]
